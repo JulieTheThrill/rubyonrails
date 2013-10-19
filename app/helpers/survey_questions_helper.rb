683b7272aef6feadd1e98570690bd4a6553dd95a
JulieTheThrill <julielgill@gmail.com>
2013-10-19 18:28:56 -0400

    Survey Questions can be either mult choice or text response

--- app/helpers/survey_questions_helper.rb
+++ app/helpers/survey_questions_helper.rb
@@ -1,2 +1,10 @@
 module SurveyQuestionsHelper
+
+  def type_options
+    types = []
+    SurveyQuestion.question_types.each do |int, text|
+      types << [text, int]
+    end
+    types
+  end
 end
--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -1,4 +1,17 @@
 class SurveyQuestion < ActiveRecord::Base
   belongs_to :survey
   has_many :survey_responses, dependent: :destroy
+
+  MULT_CHOICE = 1
+  TEXT_RESPONSE = 2
+
+  QUESTION_TYPES = {
+    MULT_CHOICE => "Multiple Choice",
+    TEXT_RESPONSE => "Text Response"
+  }
+
+  def self.question_types
+    return QUESTION_TYPES
+  end
+
 end
