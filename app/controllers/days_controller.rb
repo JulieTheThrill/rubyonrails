9220092e439e096076c465681b9f789b8b47ac80
Julie Gill <julielgill@gmail.com>
2013-10-19 13:56:19 -0400

    Initial Commit

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,9 @@
+class DaysController < ApplicationController
+
+  def welcome
+  end
+
+  def day_one
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubyonrails::Application.config.secret_key_base = 'c5bbc38a048bee7c5ed43e80126a9ffe4822236079fc779f7498aacdc82898daad92480102d508c46a8f8076d605aa3a62a67dc0b401a052e6966170795cd71c'
